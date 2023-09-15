010d4a78489312372dd539895c8d4d7e4591addb
Tapish Rathore <tapish303@gmail.com>
2023-09-15 22:51:08 +0530

    Implement pubsub listener

--- vibi-dpu/src/main.rs
+++ vibi-dpu/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+mod pubsub;
+// mod db;
+// mod core;
+// mod bitbucket;
+// mod utils;
+
+#[tokio::main]
+async fn main() {
+    // Get topic subscription and Listen to messages 
+    let gcp_credentials = //"/home/tapishr/dev-profiler/pubsub-sa.json".to_owned();
+    env::var("GCP_CREDENTIALS").expect("GCP_CREDENTIALS must be set");
+    let topic_name = //"rtapish-fromserver".to_owned();
+    env::var("INSTALL_ID").expect("INSTALL_ID must be set");
+    println!("env vars = {}, {}", &gcp_credentials, &topic_name);
+    
+    pubsub::listener::listen_messages(
+        &gcp_credentials, 
+        &topic_name,
+    ).await;
 }
