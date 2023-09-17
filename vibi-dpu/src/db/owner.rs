368a33fe20b5400a216a30654e80764a09a987fc
Tapish Rathore <tapish303@gmail.com>
2023-09-18 03:32:25 +0530

    clean up expect in save_workspace_to_db

--- vibi-dpu/src/db/owner.rs
+++ vibi-dpu/src/db/owner.rs
@@ -6,7 +6,14 @@ use crate::utils::owner::Workspace;
 pub fn save_workspace_to_db(workspace: &Workspace) {
     let uuid = workspace.uuid().clone();
     let db = get_db();
-    let json = serde_json::to_string(&workspace).expect("Failed to serialize workspace");
+    let parse_json = serde_json::to_string(&workspace);
+    if parse_json.is_err() {
+        let e = parse_json
+            .expect_err("No error in parse_json for workspace");
+        eprintln!("Failed to serialize workspace: {:?}", e);
+        return;
+    }
+    let json = parse_json.expect("Uncaught error in parse_json workspace");
     // Convert JSON string to bytes
     let bytes = json.as_bytes(); 
     // Create IVec from bytes
