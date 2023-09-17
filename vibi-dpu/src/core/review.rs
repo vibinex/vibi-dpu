d9d011790dd2c8357110055a36da73dca27c55c8
Tapish Rathore <tapish303@gmail.com>
2023-09-17 20:21:32 +0530

    break process_review function into separate functions

--- vibi-dpu/src/core/review.rs
+++ vibi-dpu/src/core/review.rs
@@ -5,30 +5,48 @@ use serde_json::Value;
 use crate::{utils::{hunk::{HunkMap, PrHunkItem}, review::Review, gitops::{commit_exists, git_pull, get_excluded_files, generate_diff, process_diffmap, generate_blame}}, db::{hunk::{get_hunk_from_db, store_hunkmap_to_db}, repo::get_clone_url_clone_dir, review::{save_review_to_db, self}}};//, core::coverage::process_coverage};
 
 pub async fn process_review(message_data: &Vec<u8>) {
-	let review_opt = get_tasks(message_data);
+	let review_opt = parse_review(message_data);
 	if review_opt.is_none() {
-		eprintln!("No review tasks found!");
+		eprintln!("Unable to deserialize review message");
 		return;
 	}
 	let review = review_opt.expect("review_opt is empty");
+	if hunk_already_exists(&review) {
+		return;
+	}
+	println!("Processing PR : {}", &review.id());
+	commit_check(&review).await;
+	let hunkmap_opt = process_review_changes(&review).await;
+	send_hunkmap(&hunkmap_opt, &review);
+}
+
+fn send_hunkmap(hunkmap_opt: &Option<HunkMap>, review: &Review) {
+	if hunkmap_opt.is_some() {
+		let hunkmap = hunkmap_opt.to_owned().expect("hunkmap_opt is empty");
+		store_hunkmap_to_db(&hunkmap, review);
+		publish_hunkmap(&hunkmap);
+		// let hunkmap_async = hunkmap.clone();
+		// process_coverage(&hunkmap_async).await; TODO - include in future PR
+	}
+}
+
+fn hunk_already_exists(review: &Review) -> bool {
 	let hunk = get_hunk_from_db(&review);
 	if hunk.is_some() {
 		let hunkval = hunk.expect("hunk is empty");
 		publish_hunkmap(&hunkval);
 		eprintln!("Hunk already in db!");
-		return;
+		return true;
 	}
+	return false;
+}
+async fn process_review_changes(review: &Review) -> Option<HunkMap>{
 	let mut prvec = Vec::<PrHunkItem>::new();
-	println!("Processing PR : {}", review.id());
-	if !commit_exists(&review.base_head_commit()) || !commit_exists(&review.pr_head_commit()) {
-		println!("Pulling repository {} for commit history", review.repo_name());
-		git_pull(&review).await;
-	}
 	let fileopt = get_excluded_files(&review);
 	println!("fileopt = {:?}", &fileopt);
 	if fileopt.is_none() {
 		eprintln!("No files to review for PR {}", review.id());
-		return;
+		return None;
 	}
 	let (_, smallfiles) = fileopt.expect("fileopt is empty");
 	let diffmap = generate_diff(&review, &smallfiles);
@@ -47,13 +65,17 @@ pub async fn process_review(message_data: &Vec<u8>) {
 		prvec,
 		format!("{}/hunkmap", review.db_key()),
 	);
-	store_hunkmap_to_db(&hunkmap, &review);
-	publish_hunkmap(&hunkmap);
-	// let hunkmap_async = hunkmap.clone();
-	// process_coverage(&hunkmap_async).await; TODO - include in future PR
+	return Some(hunkmap);
 }
 
-fn get_tasks(message_data: &Vec<u8>) -> Option<Review>{
+async fn commit_check(review: &Review) {
+	if !commit_exists(&review.base_head_commit()) || !commit_exists(&review.pr_head_commit()) {
+		println!("Pulling repository {} for commit history", &review.repo_name());
+		git_pull(review).await;
+	}
+}
+
+fn parse_review(message_data: &Vec<u8>) -> Option<Review>{
 	let data_res = serde_json::from_slice::<Value>(&message_data);
 	if data_res.is_err() {
 		let e = data_res.expect_err("No error in data_res");
