f232b5f24192f1475cf136b0aebf812e35c0a69d
Muskan Paliwal <muskan10112002@gmail.com>
2023-10-16 11:14:36 +0530

    added error handling in `fetch-access-token()` function

--- vibi-dpu/src/github/auth.rs
+++ vibi-dpu/src/github/auth.rs
@@ -1,10 +1,9 @@
 use jsonwebtoken::{encode, Header, EncodingKey, Algorithm};
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use std::env;
 use chrono::{Utc, Duration};
 use std::fs;
-use crate::client::config::get_client;
+use crate::{client::config::get_client, utils::auth::AuthInfo, };
 
 #[derive(Debug, Serialize, Deserialize)]
 struct AccessTokenResponse {
@@ -33,19 +32,37 @@ fn generate_jwt(github_app_id: &str) -> Result<String, Box<dyn std::error::Error
     Ok(token)
 }
 
-pub async fn fetch_access_token(installation_id: &str) -> Result<Value, Box<dyn std::error::Error>> {
-    let github_app_id = env::var("GITHUB_APP_ID")?;
-    let jwt_token = generate_jwt(&github_app_id)?;
+pub async fn fetch_access_token(installation_id: &str) -> Option<AuthInfo> {
+    let github_app_id = env::var("GITHUB_APP_ID").unwrap();
+    let jwt_token = generate_jwt(&github_app_id).unwrap();
 
     let client = get_client();
-    let response: Value = client.post(&format!("https://api.github.com/app/installations/{}/access_tokens", installation_id))
+    let response = client.post(&format!("https://api.github.com/app/installations/{}/access_tokens", installation_id))
         .header("Accept", "application/vnd.github+json")
         .header("Authorization", format!("Bearer {}", jwt_token))
         .header("User-Agent", "Vibinex code review Test App")
         .send()
-        .await?
-        .json()
-        .await?;
-    println!("[fetch_access_token] response = {:?}", response);
-    Ok(response)
+        .await;
+        if response.is_err() {
+            let e = response.expect_err("No error in response");
+            eprintln!("error in calling github api : {:?}", e);
+            return None;
+        }
+        let res = response.expect("Uncaught error in reponse");
+        if !res.status().is_success() {
+            println!(
+                "Failed to exchange code for access token. Status code: {}, Response content: {:?}",
+                res.status(),
+                res.text().await
+            );
+            return None;
+        }
+        let parse_res = res.json().await ;
+        if parse_res.is_err() {
+            let e = parse_res.expect_err("No error in parse_res for AuthInfo");
+            eprintln!("error deserializing AuthInfo: {:?}", e);
+            return None;
+        }
+        let response_json = parse_res.expect("Uncaught error in parse_res for AuthInfo");
+        return Some(response_json);
 }
