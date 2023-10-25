332ac0c9fc1648fa43ad32629cf8f006182e2fc9
Tapish Rathore <tapish303@gmail.com>
2023-10-25 09:21:43 +0530

    add reviewer.rs changes after merging main

--- vibi-dpu/src/core/review.rs
+++ vibi-dpu/src/core/review.rs
@@ -4,7 +4,8 @@ use serde_json::Value;
 
 use crate::{
 	utils::{hunk::{HunkMap, PrHunkItem}, 
-			review::Review, 
+			review::Review,
+			repo_config::RepoConfig, 
 			gitops::{commit_exists, 
 					git_pull, 
 					get_excluded_files, 
@@ -13,27 +14,29 @@ use crate::{
 					generate_blame}}, 
 	db::{hunk::{get_hunk_from_db, store_hunkmap_to_db}, 
 		repo::get_clone_url_clone_dir, 
-		review::{save_review_to_db}},
+		review::save_review_to_db,
+		repo_config::save_repo_config_to_db},
 	bitbucket::config::get_client,
 	core::coverage::process_coverage};
 
 pub async fn process_review(message_data: &Vec<u8>) {
 	let review_opt = parse_review(message_data);
 	if review_opt.is_none() {
-		eprintln!("Unable to deserialize review message");
+		eprintln!("Unable to deserialize review message and repo config");
 		return;
 	}
-	let review = review_opt.expect("review_opt is empty");
+	let (review, repo_config) = review_opt.expect("parse_opt is empty");
+	println!("deserialized repo_config, review = {:?}, {:?}", &repo_config, &review);
 	if hunk_already_exists(&review) {
 		return;
 	}
 	println!("Processing PR : {}", &review.id());
 	commit_check(&review).await;
 	let hunkmap_opt = process_review_changes(&review).await;
-	send_hunkmap(&hunkmap_opt, &review).await;
+	send_hunkmap(&hunkmap_opt, &review, &repo_config).await;
 }
 
-async fn send_hunkmap(hunkmap_opt: &Option<HunkMap>, review: &Review) {
+async fn send_hunkmap(hunkmap_opt: &Option<HunkMap>, review: &Review, repo_config: &RepoConfig) {
 	if hunkmap_opt.is_none() {
 		eprintln!("Empty hunkmap in send_hunkmap");
 		return;
@@ -44,7 +47,8 @@ async fn send_hunkmap(hunkmap_opt: &Option<HunkMap>, review: &Review) {
 	publish_hunkmap(&hunkmap);
 	let hunkmap_async = hunkmap.clone();
 	let review_async = review.clone();
-	process_coverage(&hunkmap_async, &review_async).await;
+	let repo_config_clone = repo_config.clone();
+	process_coverage(&hunkmap_async, &review_async, &repo_config_clone).await;
 }
 
 fn hunk_already_exists(review: &Review) -> bool {
@@ -94,29 +98,29 @@ async fn commit_check(review: &Review) {
 	}
 }
 
-fn parse_review(message_data: &Vec<u8>) -> Option<Review>{
+fn parse_review(message_data: &Vec<u8>) -> Option<(Review, RepoConfig)>{
 	let data_res = serde_json::from_slice::<Value>(&message_data);
 	if data_res.is_err() {
 		let e = data_res.expect_err("No error in data_res");
 		eprintln!("Incoming message does not contain valid reviews: {:?}", e);
 		return None;
 	}
-	let data = data_res.expect("Uncaught error in deserializing message_data");
-	println!("data == {:?}", &data["eventPayload"]["repository"]);
-	let repo_provider = data["repositoryProvider"].to_string().trim_matches('"').to_string();
-	let repo_name = data["eventPayload"]["repository"]["name"].to_string().trim_matches('"').to_string();
+	let deserialized_data = data_res.expect("Uncaught error in deserializing message_data");
+	println!("deserialized_data == {:?}", &deserialized_data["eventPayload"]["repository"]);
+	let repo_provider = deserialized_data["repositoryProvider"].to_string().trim_matches('"').to_string();
+	let repo_name = deserialized_data["eventPayload"]["repository"]["name"].to_string().trim_matches('"').to_string();
 	println!("repo NAME == {}", &repo_name);
-	let workspace_name = data["eventPayload"]["repository"]["workspace"]["slug"].to_string().trim_matches('"').to_string();
+	let workspace_name = deserialized_data["eventPayload"]["repository"]["workspace"]["slug"].to_string().trim_matches('"').to_string();
 	let clone_opt = get_clone_url_clone_dir(&repo_provider, &workspace_name, &repo_name);
 	if clone_opt.is_none() {
 		eprintln!("Unable to get clone url and directory");
 		return None;
 	}
 	let (clone_url, clone_dir) = clone_opt.expect("Empty clone_opt");
-	let pr_id = data["eventPayload"]["pullrequest"]["id"].to_string().trim_matches('"').to_string();
+	let pr_id = deserialized_data["eventPayload"]["pullrequest"]["id"].to_string().trim_matches('"').to_string();
 	let review = Review::new(
-		data["eventPayload"]["pullrequest"]["destination"]["commit"]["hash"].to_string().replace("\"", ""),
-		data["eventPayload"]["pullrequest"]["source"]["commit"]["hash"].to_string().replace("\"", ""),
+		deserialized_data["eventPayload"]["pullrequest"]["destination"]["commit"]["hash"].to_string().replace("\"", ""),
+		deserialized_data["eventPayload"]["pullrequest"]["source"]["commit"]["hash"].to_string().replace("\"", ""),
 		pr_id.clone(),
 		repo_name.clone(),
 		workspace_name.clone(),
@@ -124,11 +128,21 @@ fn parse_review(message_data: &Vec<u8>) -> Option<Review>{
 		format!("bitbucket/{}/{}/{}", &workspace_name, &repo_name, &pr_id),
 		clone_dir,
 		clone_url,
-		data["eventPayload"]["pullrequest"]["author"]["uuid"].to_string().replace("\"", ""),
+		deserialized_data["eventPayload"]["pullrequest"]["author"]["uuid"].to_string().replace("\"", ""),
 	);
 	println!("review = {:?}", &review);
 	save_review_to_db(&review);
-	return Some(review);
+	let repo_config_res = serde_json::from_value(deserialized_data["repoConfig"].clone());
+	if repo_config_res.is_err() {
+		let e = repo_config_res.expect_err("No error in repo_config_res");
+		eprintln!("Unable to deserialze repo_config_res: {:?}", e);
+		let default_config = RepoConfig::default();
+		return Some((review, default_config));
+	}
+	let repo_config = repo_config_res.expect("Uncaught error in repo_config_res");
+	println!("repo_config = {:?}", &repo_config);
+	save_repo_config_to_db(&repo_config, &review.repo_name(), &review.repo_owner(), &review.provider());
+	return Some((review, repo_config));
 }
 
 fn publish_hunkmap(hunkmap: &HunkMap) {
