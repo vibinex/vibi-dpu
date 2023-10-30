71826831c848114de275dd2ff147a3b199831c10
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-10-30 13:37:32 +0530

    1. bug fix: pubsub webhook message control flow corrected. 2. removed duplicated function (save and update pr_info functions were doing the same thing)

--- vibi-dpu/src/db/prs.rs
+++ vibi-dpu/src/db/prs.rs
@@ -3,30 +3,6 @@ use sled::IVec;
 use crate::db::config::get_db;
 use crate::utils::pr_info::PrInfo;
 
-
-pub async fn save_pr_info_to_db(workspace_slug: &str,repo_slug: &str,pr_info: PrInfo, pr_number: &str) {
-    let db = get_db();
-    let key = format!("{}/{}/{}/{}", "bitbucket", workspace_slug, repo_slug, pr_number);
-
-    let pr_info_bytes = serde_json::to_vec(&pr_info);
-    if pr_info_bytes.is_err() {
-        let e = pr_info_bytes.expect_err("Empty error in pr_info_bytes");
-		eprintln!("Unable to serialize pr info: {:?}, error: {:?}", pr_info, e);
-		return;
-    }
-    let pr_info_json = pr_info_bytes.expect("Uncaught error in parse_res repo");
-
-    let insert_result = db.insert(key.as_bytes(), IVec::from(pr_info_json)); 
-    if insert_result.is_err() {
-        let e = insert_result.expect_err("No error in inserting pr_info");
-        eprintln!("Failed to insert PR info into the database. {:?}", e);
-        return;
-    }
-
-    println!("PR succesfully upserted: {:?} {:?}", key, pr_info);
-}
-
-
 pub async fn update_pr_info_in_db(workspace_slug: &str, repo_slug: &str, pr_info: &PrInfo, pr_number: &str) {
     let key = format!("{}/{}/{}/{}", "bitbucket", workspace_slug, repo_slug, pr_number);
     let db = get_db();
@@ -65,7 +41,8 @@ pub async fn process_and_update_pr_if_different(webhook_data: &Value, workspace_
     let pr_info_db_opt = get_pr_info_from_db(workspace_slug, repo_slug, pr_number, repo_provider, &pr_info_parsed).await;
     if pr_info_db_opt.is_none() {
         eprintln!("[process_and_update_pr_if_different] No pr_info in db, parsed: {:?}", pr_info_parsed);
-        return false;
+        update_pr_info_in_db(&workspace_slug, &repo_slug, &pr_info_parsed, &pr_number).await;
+        return true; // new pr
     }
     let pr_info_db = pr_info_db_opt.expect("Empty pr_info_db_opt");
     if pr_info_db.pr_head_commit().to_string().eq_ignore_ascii_case(pr_info_parsed.pr_head_commit()){
