9ace978aa7483f9830ad947bad6bb2dfcabbe04d
Muskan Paliwal <muskan10112002@gmail.com>
2023-10-31 11:50:46 +0530

    created a comon trait AuthInfo for bith github and bitbucket

--- vibi-dpu/src/utils/auth.rs
+++ vibi-dpu/src/utils/auth.rs
@@ -1,18 +1,25 @@
 use serde::Deserialize;
 use serde::Serialize;
 
+use super::user::BitbucketUser;
+
+pub trait AuthInfo {
+    fn access_token(&self) -> &str;
+    fn timestamp(&self) -> Option<u64>;
+    fn set_timestamp(&mut self, timestamp: u64);
+}
+
 #[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct AuthInfo {
+pub struct BitbucketAuthInfo {
     access_token: String,
     refresh_token: String,
     expires_in: u64,
     timestamp: Option<u64>,
 }
 
-impl AuthInfo {
-    // Constructor
-    pub fn new(access_token: String, refresh_token: String, expires_in: u64, timestamp: Option<u64>) -> Self {
-        Self {
+impl BitbucketAuthInfo{
+    pub fn new(access_token: String, refresh_token: String, timestamp: Option<u64>, expires_in: u64) -> Self {
+        Self{
             access_token,
             refresh_token,
             expires_in,
@@ -20,25 +27,65 @@ impl AuthInfo {
         }
     }
 
-    // Public getter methods
-    pub fn access_token(&self) -> &String {
-        &self.access_token
-    }
-
     pub fn refresh_token(&self) -> &String {
         &self.refresh_token
     }
 
-    pub fn expires_in(&self) -> u64 {
-        self.expires_in
+    pub fn expires_in(&self) -> &u64 {
+        &self.expires_in
+    }
+}
+
+impl AuthInfo for BitbucketAuthInfo {
+    // Public getter methods
+    fn access_token(&self) -> &String {
+        &self.access_token
     }
 
-    pub fn timestamp(&self) -> &Option<u64> {
+    fn timestamp(&self) -> &Option<u64> {
         &self.timestamp
     }
 
     // Public setters
-    pub fn set_timestamp(&mut self, timestamp: u64) {
+    fn set_timestamp(&mut self, timestamp: u64) {
         self.timestamp = Some(timestamp);
+    }
+}
+
+pub struct GithubAuthInfo {
+    access_token: String,
+    install_id: String,
+    expires_at: u64,
+    timestamp: Option<u64>,
+}
+
+impl GithubAuthInfo {
+    pub fn new(access_token: String, install_id: String, expires_at: u64, timestamp: Option<u64>) -> Self {
+        Self {
+            access_token,
+            install_id,
+            expires_at,
+            timestamp
+        }
+    }
+
+    pub fn install_id(&self) -> &String {
+        &self.install_id
+    }
+
+    pub fn expires_at(&self) -> &u64 {
+        &self.expires_at
+    }
+}
+
+impl AuthInfo for GithubAuthInfo {
+
+    //getter methods
+    fn access_token(&self) -> &String {
+        &self.access_token
+    }
+
+    fn timestamp(&self) -> &Option<u64> {
+        &self.timestamp
     }
 }
