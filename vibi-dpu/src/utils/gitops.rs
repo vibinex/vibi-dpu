04a29a141540abb0c7da4568ee8e4bbf6ce7af2d
Tapish Rathore <tapish303@gmail.com>
2023-11-22 10:03:54 +0530

    add raw filepath to get blame user

--- vibi-dpu/src/utils/gitops.rs
+++ vibi-dpu/src/utils/gitops.rs
@@ -426,7 +426,8 @@ async fn process_blameitem(path: &str, linenum: &str, blamelines: Vec<&str>) -> 
 					linebreak.to_string(),
 					lidx.to_string(),
 					digest(path),
-					lineitem.commit().trim_matches('"').to_string())
+					lineitem.commit().trim_matches('"').to_string(),
+					path.to_string())
 				);
 				linebreak = lidx + 1;
 			}
@@ -441,7 +442,9 @@ async fn process_blameitem(path: &str, linenum: &str, blamelines: Vec<&str>) -> 
 			linebreak.to_string(),
 			lastidx.to_string(),
 			digest(path),
-			lineitem.commit().to_string()));
+			lineitem.commit().to_string(),
+			path.to_string(),
+		));
 	}
 	return Some(blamevec);
 }
