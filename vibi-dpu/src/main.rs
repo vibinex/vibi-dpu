06f3bcbd07eb2c9e8af43ec147624f874d9a0935
Muskan Paliwal <muskan10112002@gmail.com>
2024-01-25 09:59:38 +0530

    created a function named `setup_self_host_user_repos_github` similar to `handle_install_github` and used it in `main.rs` if `PAt` and `PROVIDER` in env are set

--- vibi-dpu/src/main.rs
+++ vibi-dpu/src/main.rs
@@ -6,6 +6,7 @@ mod bitbucket;
 mod github;
 mod utils;
 mod logger;
+use tokio::task;
 
 #[tokio::main]
 async fn main() {
@@ -14,10 +15,23 @@ async fn main() {
     env::var("GCP_CREDENTIALS").expect("GCP_CREDENTIALS must be set");
     let topic_name = //"rtapish-fromserver".to_owned();
     env::var("INSTALL_ID").expect("INSTALL_ID must be set");
-
+    
     let logs_init_status = logger::init::init_logger();
     if !logs_init_status {
         log::error!("[main] Unable to file logger");
+    }
+
+    let pat_env_var = "GITHUB_PAT";
+    let provider_env_var = "PROVIDER";
+    if let (Ok(pat), Ok(provider)) = (env::var(pat_env_var), env::var(provider_env_var)) {
+        log::info!("[main] Personal Access Token: {}", pat);
+        log::info!("[main] Provider: {}", provider);
+        if provider == "GITHUB" {
+            task::spawn(async move {
+                core::github::setup::setup_self_host_user_repos_github(&pat).await;
+                log::info!("[main] Github repos self host setup processed");
+            });    
+        }
     }
     log::info!("[main] env vars = {}, {}", &gcp_credentials, &topic_name);
     pubsub::listener::listen_messages(
