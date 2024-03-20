687d948978bce804667d45a25269cf369098b714
Tapish Rathore <tapish@vibinex.com>
2024-03-20 18:18:40 +0530

    fix process_trigger steps

--- vibi-dpu/src/core/trigger.rs
+++ vibi-dpu/src/core/trigger.rs
@@ -1,6 +1,8 @@
 pub async fn process_trigger(message_data: &Vec<u8>) {
+    // parse message
+    // create review object from db
     // get access token
-    // get pr information and create review object
+    // get pr information and update review object
     // commit_check
     // process_review_changes
     // send_hunkmap
