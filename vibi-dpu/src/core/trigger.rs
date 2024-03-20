45477d364c66672f44b8c8627f327ae196c996a4
Tapish Rathore <tapish@vibinex.com>
2024-03-20 18:13:52 +0530

    Add process trigger function

---
+++ vibi-dpu/src/core/trigger.rs
@@ -0,0 +1,7 @@
+pub async fn process_trigger(message_data: &Vec<u8>) {
+    // get access token
+    // get pr information and create review object
+    // commit_check
+    // process_review_changes
+    // send_hunkmap
+}
