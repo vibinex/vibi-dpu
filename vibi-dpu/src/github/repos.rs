d1e7726d3fc1be23980c4b781a8c1b950c67d62b
Tapish Rathore <tapish@vibinex.com>
2024-03-31 23:57:36 +0530

    remove format and apply only tabs

--- vibi-dpu/src/github/repos.rs
+++ vibi-dpu/src/github/repos.rs
@@ -24,11 +24,8 @@ pub async fn get_github_app_installed_repos(access_token: &str) -> Option<Vec<Re
 	}
 	let repos_val = repos_opt.expect("Empty repos_opt");
 	let repositories = deserialize_repos(repos_val);
-	log::debug!(
-		"[get_github_app_installed_repos] Fetched {:?} repositories from GitHub",
-		&repositories
-	);
-	return Some(repositories);
+	log::debug!("[get_github_app_installed_repos] Fetched {:?} repositories from GitHub", &repositories);
+	return Some(repositories)
 }
 
 pub async fn get_user_accessed_github_repos(access_token: &str) -> Option<Vec<Repository>> {
@@ -49,21 +46,16 @@ pub async fn get_user_accessed_github_repos(access_token: &str) -> Option<Vec<Re
 	}
 	let selected_repositories = selected_repositories_opt.expect("EMpty selected_repositories_opt");
 	// Set union of all and user selected repos
-	let pat_repos: Vec<Repository> = all_repos
-		.into_iter()
-		.filter(|repo| {
-			selected_repositories.iter().any(|selected_repo| {
-				repo.name() == &selected_repo.repo_name
-					&& repo.provider() == &selected_repo.repo_provider
-					&& repo.owner() == &selected_repo.repo_owner
-			})
+	let pat_repos: Vec<Repository> = all_repos.into_iter().filter(|repo| {
+		selected_repositories.iter().any(|selected_repo| {
+			repo.name() == &selected_repo.repo_name &&
+			repo.provider() == &selected_repo.repo_provider &&
+			repo.owner() == &selected_repo.repo_owner
 		})
-		.collect();
-	log::debug!(
-		"[get_user_accessed_github_repos] Fetched {:?} repositories from GitHub",
-		&pat_repos
-	);
-	return Some(pat_repos);
+	})
+	.collect();
+	log::debug!("[get_user_accessed_github_repos] Fetched {:?} repositories from GitHub", &pat_repos);
+	return Some(pat_repos)
 }
 
 async fn user_selected_repos() -> Option<Vec<UserSelectedRepo>> {
@@ -71,23 +63,18 @@ async fn user_selected_repos() -> Option<Vec<UserSelectedRepo>> {
 	let topic_name = env::var("INSTALL_ID").expect("INSTALL_ID must be set");
 	let provider = env::var("PROVIDER").expect("PROVIDER must be set");
 	let server_prefix_url = env::var("SERVER_URL").expect("SERVER_URL must be set");
-	let url = format!(
-		"{}/api/dpu/repos?topicId={}&provider={}",
-		&server_prefix_url, &topic_name, &provider
-	);
+	let url = format!("{}/api/dpu/repos?topicId={}&provider={}",
+			&server_prefix_url, &topic_name, &provider);
 	let repos_res = client.get(url).send().await;
 	if let Err(e) = repos_res {
-		log::error!(
-			"[user_selected_repos] Unable to get repos from server, {:?}",
-			e
-		);
+		log::error!("[user_selected_repos] Unable to get repos from server, {:?}", e);
 		return None;
 	}
 	let repos_response = repos_res.expect("Uncaught error in repos_res");
 	return parse_repos_response(repos_response).await;
 }
 
-async fn parse_repos_response(response: Response) -> Option<Vec<UserSelectedRepo>> {
+async fn parse_repos_response(response: Response) -> Option<Vec<UserSelectedRepo>>{
 	let repos_value_res = response.json::<Value>().await;
 	if let Err(e) = &repos_value_res {
 		log::error!("[parse_repos_response] Unable to parse response {:?}", e);
@@ -96,23 +83,16 @@ async fn parse_repos_response(response: Response) -> Option<Vec<UserSelectedRepo
 	let repos_value = repos_value_res.expect("Uncaught error in repos_values_res");
 	let repolist_json_opt = repos_value.get("repoList");
 	if repolist_json_opt.is_none() {
-		log::error!(
-			"[parse_repos_response] No repoList key in response json: {:?}",
-			&repos_value
-		);
+		log::error!("[parse_repos_response] No repoList key in response json: {:?}", &repos_value);
 		return None;
 	}
 	let repolist_json = repolist_json_opt.expect("Uncaught error in repolist_json_opt");
 	let repolist_vec_res = serde_json::from_value(repolist_json.clone());
 	if let Err(e) = &repolist_vec_res {
-		log::error!(
-			"[parse_repos_response] Unable to parse vec of UserSelectedRepo from response: {:?}",
-			e
-		);
+		log::error!("[parse_repos_response] Unable to parse vec of UserSelectedRepo from response: {:?}", e);
 		return None;
 	}
-	let repolist_vec: Vec<UserSelectedRepo> =
-		repolist_vec_res.expect("Uncaught error in repolist_vec_res");
+	let repolist_vec: Vec<UserSelectedRepo> = repolist_vec_res.expect("Uncaught error in repolist_vec_res");
 	return Some(repolist_vec);
 }
 
@@ -121,10 +101,7 @@ fn deserialize_repos(repos_val: Vec<Value>) -> Vec<Repository> {
 	for response_json in repos_val {
 		let repo_json_opt = response_json["repositories"].as_array();
 		if repo_json_opt.is_none() {
-			log::error!(
-				"[deserialize_repos] Unable to deserialize repo value: {:?}",
-				&response_json
-			);
+			log::error!("[deserialize_repos] Unable to deserialize repo value: {:?}", &response_json);
 			continue;
 		}
 		let repos_page_json = repo_json_opt.expect("Empty repo_json_opt").to_owned();
@@ -142,10 +119,7 @@ fn deserialise_github_pat_repos(repos_val: Vec<Value>) -> Vec<Repository> {
 	for response_json in repos_val {
 		let repo_json_opt = response_json.as_array();
 		if repo_json_opt.is_none() {
-			log::error!(
-				"[deserialize_repos] Unable to deserialize repo value: {:?}",
-				&response_json
-			);
+			log::error!("[deserialize_repos] Unable to deserialize repo value: {:?}", &response_json);
 			continue;
 		}
 		let repos_page_json = repo_json_opt.expect("Empty repo_json_opt").to_owned();
@@ -167,20 +141,11 @@ fn deserialize_repo_object(repo_json: &Value) -> Repository {
 	let repo = Repository::new(
 		repo_json["name"].to_string().trim_matches('"').to_string(),
 		repo_json["id"].to_string().trim_matches('"').to_string(),
-		repo_json["owner"]["login"]
-			.to_string()
-			.trim_matches('"')
-			.to_string(),
+		repo_json["owner"]["login"].to_string().trim_matches('"').to_string(),
 		is_private,
-		repo_json["ssh_url"]
-			.to_string()
-			.trim_matches('"')
-			.to_string(),
-		None,
-		repo_json["owner"]["login"]
-			.to_string()
-			.trim_matches('"')
-			.to_string(),
+		repo_json["ssh_url"].to_string().trim_matches('"').to_string(),
+		None,    
+		repo_json["owner"]["login"].to_string().trim_matches('"').to_string(),
 		None,
 		"github".to_string(),
 	);
