90e1680128daa6e9dfa11ef530044e74a3f68edd
Tapish Rathore <tapish@vibinex.com>
2024-04-01 09:37:52 +0530

    modularise code and add parse_field function

--- vibi-dpu/src/core/trigger.rs
+++ vibi-dpu/src/core/trigger.rs
@@ -18,6 +18,25 @@ pub async fn process_trigger(message_data: &Vec<u8>) {
     // send_hunkmap
 }
 
+fn parse_field(field_name: &str, msg: &Value) -> Option<String> {
+    let field_val_opt = msg.get(field_name);
+    if field_val_opt.is_none() {
+        log::error!("[parse_field] {} not found in {}", field_name, msg);
+        return None;
+    }
+    let field_val = field_val_opt.expect("Empty field_val_opt");
+    return Some(field_val.to_string().trim_matches('"').to_string());
+}
+
+fn parse_message_fields(msg: &Value) ->
+    (Option<String>, Option<String>, Option<String>, Option<String>) {
+
+    let repo_provider_opt = parse_field("repo_provider", msg);
+    let repo_owner_opt = parse_field("repo_owner", msg);
+    let repo_name_opt = parse_field("repo_name", msg);
+    let pr_number_opt = parse_field("pr_number", msg);
+    return (repo_provider_opt, repo_owner_opt, repo_name_opt, pr_number_opt);
+}
 
 fn parse_trigger_msg(message_data: &Vec<u8>) -> Option<(Review, RepoConfig)> {
 	let data_res = serde_json::from_slice::<Value>(&message_data);
@@ -28,10 +47,15 @@ fn parse_trigger_msg(message_data: &Vec<u8>) -> Option<(Review, RepoConfig)> {
 	}
 	let deserialized_data = data_res.expect("Uncaught error in deserializing message_data");
 	log::debug!("[parse_trigger_msg] deserialized_data == {:?}", &deserialized_data);
-	let repo_provider = deserialized_data["repo_provider"].to_string().trim_matches('"').to_string();
-    let repo_owner = deserialized_data["repo_owner"].to_string().trim_matches('"').to_string();
-    let repo_name = deserialized_data["repo_name"].to_string().trim_matches('"').to_string();
-    let pr_number = deserialized_data["pr_number"].to_string().trim_matches('"').to_string();
+	let (repo_provider_opt, repo_owner_opt, repo_name_opt, pr_number_opt) = parse_message_fields(&deserialized_data);
+    if repo_provider_opt.is_none() || repo_owner_opt.is_none() || repo_name_opt.is_none() || pr_number_opt.is_none() {
+        log::error!("[parse_trigger_msg] Could not parse {:?}, {:?}, {:?}, {:?}", repo_provider_opt, repo_owner_opt, repo_name_opt, pr_number_opt);
+        return None;
+    }
+    let repo_provider = repo_provider_opt.expect("Empty repo_provider_opt");
+    let repo_owner = repo_owner_opt.expect("Empty repo_provider_opt");
+    let repo_name = repo_name_opt.expect("Empty repo_provider_opt");
+    let pr_number = pr_number_opt.expect("Empty repo_provider_opt");
     let repo_config_res = serde_json::from_value(deserialized_data["repo_config"].clone());
     if let Err(e) = &repo_config_res {
         log::error!("[parse_trigger_msg] Error in parsing repo config: {:?}", e);
