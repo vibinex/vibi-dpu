ded6b794b0f5437be7bba72eeae56d3b1e6c8009
MuskanPaliwal <muskan10112002@gmail.com>
2024-04-23 17:11:49 +0530

    created another `user-deploy-cloud-pat-script.sh` bash script to include `github_pat` and `provider` env vars for cloud-individual-pat flow

---
+++ user-deploy-cloud-pat-script.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+# Ensure variables are set
+if [ -z "$PROJECT_ID" ] || [ -z "$SHORT_SHA" ] || [ -z "$_USER_ID" ] || [ -z "$_INSTALL_ID" ] || [ -z "$_GITHUB_PAT" ] || [ -z "$_PROVIDER" ]; then
+	echo "Error: Environment variables not set."
+	exit 1
+fi
+
+# Concatenate variables to create deployment and container names
+DEPLOYMENT_NAME="dpu-deployment-${SHORT_SHA}-${_USER_ID}"
+CONTAINER_NAME="dpu-container-${SHORT_SHA}-${_USER_ID}"
+
+# Generate the deployment configuration file
+cat <<EOF > generated-dpu-deployment.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: $DEPLOYMENT_NAME
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: dpu
+  template:
+    metadata:
+      labels:
+      app: dpu
+    spec:
+      containers:
+      - name: $CONTAINER_NAME
+      image: asia.gcr.io/$PROJECT_ID/dpu/dpu:$SHORT_SHA
+      ports:
+      - containerPort: 80
+      env:
+      - name: INSTALL_ID
+        value: $_INSTALL_ID
+      - name: GITHUB_PAT
+        value: $_GITHUB_PAT
+      -name: PROVIDER
+        value: $_PROVIDER	
+EOF
+
+# Display the generated YAML for debugging
+echo "Generated YAML:"
+cat generated-dpu-deployment.yaml
