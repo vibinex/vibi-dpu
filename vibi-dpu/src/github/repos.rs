4a66f3c9e224c9450a14d16608ec0f0e9432b53b
MuskanPaliwal <muskan10112002@gmail.com>
2024-05-09 15:25:41 +0530

    next version of graphql api

--- vibi-dpu/src/github/repos.rs
+++ vibi-dpu/src/github/repos.rs
@@ -1,8 +1,11 @@
+use std::borrow::Borrow;
+
 use serde_json::Value;
 use reqwest::Client;
 use serde_json::json;
 
 use super::config::{get_api_paginated, github_base_url};
+use crate::bitbucket::repo;
 use crate::{core::utils::user_selected_repos, utils::reqwest_client::get_client};
 use crate::utils::repo::Repository;
 use crate::db::repo::save_repo_to_db;
@@ -86,52 +89,83 @@ pub async fn get_user_accessed_github_repos(access_token: &str) -> Option<Vec<Re
 	return Some(pat_repos)
 }
 
+fn conditional_end_cursor (end_cursor: &str) -> String {
+    if end_cursor == "null" {
+       return "null".to_string();
+    } else {
+        return format!("\"{}\"", end_cursor);
+    }
+}
 
 pub async fn get_user_github_repos_using_graphql_api(
 	access_token: &str,
 ) -> Option<Vec<Repository>> {
 	let client = get_client();
 	let pageCount = "100";
+    let mut has_next_page = true;
+    let mut end_cursor = "null".to_string();
+    
+    let mut all_repositories = Vec::<Repository>::new();
+    
+    while has_next_page {
+        log::debug!("[get_user_github_repos_using_graphql_api] has_next_page while starting: {}", has_next_page);
+        log::debug!("[get_user_github_repos_using_graphql_api] end_cursor while starting: {}", end_cursor);
 
-    let query = format!("query {{ viewer {{ repositories(first: {pageCount}, affiliations: [OWNER, ORGANIZATION_MEMBER, COLLABORATOR], ownerAffiliations: [OWNER, ORGANIZATION_MEMBER, COLLABORATOR]) {{ totalCount nodes {{ name id isPrivate sshUrl owner {{ login }} }} }} }} }}");
-    let body = json!({
-        "query": query
-    });
-
-    log::debug!("[get_user_github_repos_using_graphql_api] Executing GraphQL query: {:?}", body);
-    let graphql_request = client
-        .post("https://api.github.com/graphql")
-        .header("Authorization", format!("Bearer {access_token}")) 
-        .header("Content-Type", "application/json")
-        .header("User-Agent", "vibi-dpu")
-        .json(&body).build().unwrap();
-    log::debug!("[get_user_github_repos_using_graphql_api] Request Headers: {:?}", graphql_request.headers());
-    log::debug!("[get_user_github_repos_using_graphql_api] Request URL: {:?}", graphql_request.url());
-    let response = client.execute(graphql_request)
-        .await
-        .expect("Failed to execute request");
-
-    let status = response.status();
-    log::debug!("[get_user_github_repos_using_graphql_api] status = {status}");
-    let resp_val_res = response.json::<Value>().await;
-    if resp_val_res.is_err() {
-    let err = resp_val_res.expect_err("No error in resp_val_res"); 
-    log::error!("[get_user_github_repos_using_graphql_api] error in parsing json: {:?}", &err);
-    return None;
-    }
-    let resp_val = resp_val_res.expect("Uncaught error in resp_val_res");
-    let nodes_vec_res = serde_json::from_value(
-        resp_val["data"]["viewer"]["repositories"]["nodes"].clone());
-        if nodes_vec_res.is_err(){
-            let err = nodes_vec_res.expect_err("Empty error in nodes_vec_res");
-            log::error!("[get_user_github_repos_using_graphql_api] unable to parse nodes from json: {:?}", &err);
-            return None;
+        let query = format!("query {{ viewer {{ repositories(first: {pageCount}, after: {:?}, affiliations: [OWNER, ORGANIZATION_MEMBER, COLLABORATOR], ownerAffiliations: [OWNER, ORGANIZATION_MEMBER, COLLABORATOR]) {{ totalCount nodes {{ name id isPrivate sshUrl owner {{ login }} }} pageInfo {{ hasNextPage endCursor }} }} }} }}",
+        conditional_end_cursor(&end_cursor));
+        let body = json!({
+            "query": query
+        });
+    
+        log::debug!("[get_user_github_repos_using_graphql_api] Executing GraphQL query: {:?}", body);
+        let graphql_request = client
+            .post("https://api.github.com/graphql")
+            .header("Authorization", format!("Bearer {access_token}")) 
+            .header("Content-Type", "application/json")
+            .header("User-Agent", "vibi-dpu")
+            .json(&body).build().unwrap();
+        log::debug!("[get_user_github_repos_using_graphql_api] Request Headers: {:?}", graphql_request.headers());
+        log::debug!("[get_user_github_repos_using_graphql_api] Request URL: {:?}", graphql_request.url());
+        let response = client.execute(graphql_request)
+            .await
+            .expect("Failed to execute request");
+    
+        let status = response.status();
+        log::debug!("[get_user_github_repos_using_graphql_api] status = {status}");
+        let text_body = response.text().await.expect("Failed to get response body");
+        log::debug!("[get_user_github_repos_using_graphql_api] Response Body: {:?}", text_body);
+        let resp_val_res = serde_json::from_str(&text_body);
+        if resp_val_res.is_err() {
+        let err = resp_val_res.expect_err("No error in resp_val_res"); 
+        log::error!("[get_user_github_repos_using_graphql_api] error in parsing json: {:?}", &err);
+        return None;
         }
-        let nodes_vec: Vec<Value> = nodes_vec_res.expect("Uncaught error in nodes_)vec_res"); 
-        let repos = deserialize_repos_object_graphql(&nodes_vec);
-        
-        log::debug!("[get_user_github_repos_using_graphql_api] repos = {:?}", &repos);
-        return Some(repos);
+        let resp_val: Value = resp_val_res.expect("Uncaught error in resp_val_res");
+        let nodes_vec_res = serde_json::from_value(
+            resp_val["data"]["viewer"]["repositories"]["nodes"].clone());
+            if nodes_vec_res.is_err(){
+                let err = nodes_vec_res.expect_err("Empty error in nodes_vec_res");
+                log::error!("[get_user_github_repos_using_graphql_api] unable to parse nodes from json: {:?}", &err);
+                return None;
+            }
+            let nodes_vec: Vec<Value> = nodes_vec_res.expect("Uncaught error in nodes_)vec_res"); 
+            let repos = deserialize_repos_object_graphql(&nodes_vec);
+            all_repositories.extend(repos);
+            log::debug!("[get_user_github_repos_using_graphql_api] Fetched {:?} repositories from GitHub", all_repositories);
+
+            end_cursor = resp_val["data"]["viewer"]["repositories"]["pageInfo"]["endCursor"].to_string().trim_matches('"').to_string();
+            log::debug!("[get_user_github_repos_using_graphql_api] end_cursor = {}", end_cursor);
+            let has_next_page_opt = resp_val["data"]["viewer"]["repositories"]["pageInfo"]["hasNextPage"].as_bool().clone();
+            log::debug!("[get_user_github_repos_using_graphql_api] has_next_page_opt = {:?}", has_next_page_opt);
+            if has_next_page_opt.is_none(){
+                log::error!("[get_user_github_repos_using_graphql_api] unable to parse hasNextPage from json");
+                break;
+            }
+            has_next_page = has_next_page_opt.expect("Unable to parse hasNextPage from JSON");
+            log::debug!("[get_user_github_repos_using_graphql_api] has_next_page = {}", has_next_page);
+    }
+        log::debug!("[get_user_github_repos_using_graphql_api] repos = {:?}", &all_repositories);
+        return Some(all_repositories);
 }
 
 fn deserialize_repos_object_graphql(repos_json: &Vec<Value>) -> Vec<Repository> {
