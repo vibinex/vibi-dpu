8241f35154b74a9057f99eb6610faca5ea2611a8
Tapish Rathore <tapish@vibinex.com>
2024-10-13 04:27:23 +0530

    fix func color and skip repeating funcs

--- vibi-dpu/src/graph/graph_edges.rs
+++ vibi-dpu/src/graph/graph_edges.rs
@@ -1,4 +1,4 @@
-use std::path::{Path, PathBuf};
+use std::{collections::HashSet, path::{Path, PathBuf}};
 use crate::utils::{gitops::git_checkout_commit, review::Review};
 
 use super::{elements::MermaidGraphElements, file_imports::ImportIdentifier, function_call::{function_calls_search, FunctionCallIdentifier}, function_line_range::{generate_function_map, get_function_def_for_func_call, FunctionDefIdentifier}, graph_info::DiffGraph, utils::{absolute_to_relative_path, detect_language}};
@@ -9,185 +9,6 @@ pub async fn graph_edges(base_filepaths: &Vec<PathBuf>, review: &Review, diff_gr
 }
 
 async fn incoming_edges(review: &Review, diff_graph: &DiffGraph, graph_elems: &mut MermaidGraphElements) {
-    // filter files with ripgrep
-    // for each filtered file
-        // get func call
-        // get func def
-    // for (dest_filename, func_defs) in diff_graph.diff_func_defs() {
-    //     let mut dest_file_rel = dest_filename.to_string();
-    //     if let Some(dest_file_relative_path) = absolute_to_relative_path(&dest_filename, review) {
-    //         dest_file_rel = dest_file_relative_path;
-    //     }
-    //     let all_files: Vec<&String> = diff_graph.diff_func_defs().keys().collect();    
-        //     for (source_filename, file_func_defs) in diff_graph.all_file_imports().file_import_map() {
-        //         let mut source_rel_path = source_filename.to_string();
-        //         if let Some(src_relative_filepath) = absolute_to_relative_path(&source_rel_path, review) {
-        //             source_rel_path = src_relative_filepath;
-        //         }
-        //         let file_imports = file_func_defs.all_import_paths();
-        //         for file_import in file_imports {
-        //             // search for correct import
-        //             if let Some(dest_filepath) = match_imported_filename_to_path(head_filepaths, &file_import.import_path()) {
-        //                 if match_import_func(&file_import, dest_func) {
-        //                     // find func call
-        //                     let src_filepath = PathBuf::from_str(source_filename).expect("Unable to create pathbuf");
-        //                     // TODO, FIXME - function_calls_in_file should have src_filename or src_filepath? - check other calls to the function as well
-        //                     if let Some(func_call_vec) = function_calls_in_file(&src_filepath, dest_func.name()).await {
-        //                         // call func in  that takes vec of lines and returns funcdefs
-        //                         let lines = func_call_vec.iter().flat_map(|chunk| chunk.function_calls()).cloned().collect();
-        //                         let source_func_defs = diff_graph.all_file_func_defs().functions_in_file(source_filename).expect("No source filename found").funcs_for_lines(&lines);
-        //                         for (line_num, source_func_def) in source_func_defs {
-        //                             if source_func_def != dest_func.to_owned() {
-        //                                 graph_elems.add_edge("",
-        //                                     line_num.to_owned(), 
-        //                                     &source_func_def.name(), 
-        //                                     &dest_func.name(),
-        //                                     &source_rel_path,
-        //                                     &dest_file_rel,
-        //                                     "",
-        //                                     "green",
-        //                                     source_func_def.line_start(),
-        //                                     dest_func.line_start()
-        //                                 );
-        //                             }
-        //                         }
-        //                     }
-        //                 }    
-        //             }
-        //         }
-        //     }
-            
-        //     // search in full graph
-        //     for (source_filename, file_func_defs) in all_import_info.file_import_map() {
-        //         let mut source_file_rel = source_filename.to_string();
-        //         if let Some(src_relative_filepath) = absolute_to_relative_path(&source_file_rel, review) {
-        //             source_file_rel = src_relative_filepath;
-        //         } 
-        //         let file_imports = file_func_defs.all_import_paths();
-        //         for file_import in file_imports {
-        //             // search for correct import
-        //             if let Some(dest_filepath) = match_imported_filename_to_path(head_filepaths, file_import.import_path()) {
-        //                 if match_import_func(&file_import, dest_func) {
-        //                     // if found, create edge
-        //                     let src_filepath = PathBuf::from_str(source_filename).expect("Unable to create pathbuf");
-        //                     if let Some(func_call_vec) = function_calls_in_file(&src_filepath, dest_func.name()).await {
-        //                         // call func in  that takes vec of lines and returns funcdefs
-        //                         let lines = func_call_vec.iter().flat_map(|chunk| chunk.function_calls()).cloned().collect();
-        //                         let source_func_defs_opt = diff_graph.all_file_func_defs().functions_in_file(source_filename);
-        //                         if source_func_defs_opt.is_none() {
-        //                             log::debug!("[incoming_edges] No funcs for file: {}", source_filename);
-        //                             continue;
-        //                         }
-        //                         let source_func_defs = source_func_defs_opt.expect("No source filename found").funcs_for_lines(&lines);
-        //                         for (line_num, source_func_def) in source_func_defs {
-        //                             if source_func_def != dest_func.to_owned() {
-        //                                 graph_elems.add_edge("",
-        //                                     line_num.to_owned(), 
-        //                                     &source_func_def.name(), 
-        //                                     &dest_func.name(),
-        //                                     &source_file_rel,
-        //                                     &dest_file_rel,
-        //                                     "",
-        //                                     "green",
-        //                                     source_func_def.line_start(),
-        //                                     dest_func.line_start()
-        //                                 );
-        //                             }
-        //                         }
-        //                     }
-        //                 }    
-        //             }
-        //         }
-        //     } 
-        // }
-        // for dest_func in func_defs.deleted_func_defs() {
-        //     // search in diff graph
-        //     for (source_filename, file_func_defs) in diff_graph.all_file_imports().file_import_map() {
-        //         let mut source_file_rel = source_filename.to_string();
-        //         if let Some(src_relative_filepath) = absolute_to_relative_path(&source_file_rel, review) {
-        //             source_file_rel = src_relative_filepath;
-        //         }
-        //         let file_imports = file_func_defs.all_import_paths();
-        //         for file_import in file_imports {
-        //             // search for correct import
-        //             if let Some(dest_filepath) = match_imported_filename_to_path(head_filepaths, file_import.import_path()) {
-        //                 if match_import_func(&file_import, dest_func) {
-        //                     // find func call
-        //                     git_checkout_commit(review, review.pr_head_commit());
-        //                     let src_filepath = PathBuf::from_str(source_filename).expect("Unable to create pathbuf");
-        //                     if let Some(func_call_vec) = function_calls_in_file(&src_filepath, dest_func.name()).await {
-        //                         // call func in  that takes vec of lines and returns funcdefs
-        //                         let lines = func_call_vec.iter().flat_map(|chunk| chunk.function_calls()).cloned().collect();
-        //                         let source_func_defs_opt = diff_graph.all_file_func_defs().functions_in_file(source_filename);
-        //                         if source_func_defs_opt.is_none() {
-        //                             log::debug!("[incoming_edges] No funcs for file: {}", source_filename);
-        //                             continue;
-        //                         }
-        //                         let source_func_defs = source_func_defs_opt.expect("No source filename found").funcs_for_lines(&lines);
-        //                         for (line_num, source_func_def) in source_func_defs {
-        //                             if source_func_def != dest_func.to_owned() {
-        //                                 graph_elems.add_edge("",
-        //                                     line_num.to_owned(), 
-        //                                     &source_func_def.name(), 
-        //                                     &dest_func.name(),
-        //                                     &source_file_rel,
-        //                                     &dest_file_rel,
-        //                                     "",
-        //                                     "red",
-        //                                     source_func_def.line_start(),
-        //                                     dest_func.line_start()
-        //                                 );
-        //                             }
-        //                         }
-        //                     }
-        //                 }    
-        //             }
-        //         }
-        //     }
-        //     // search in full graph
-        //     for (source_filename, file_func_defs) in all_import_info.file_import_map() {
-        //         let mut source_file_rel = source_filename.to_string();
-        //         if let Some(src_relative_filepath) = absolute_to_relative_path(&source_file_rel, review) {
-        //             source_file_rel = src_relative_filepath;
-        //         }
-        //         let file_imports = file_func_defs.all_import_paths();
-        //         for file_import in file_imports {
-        //             // search for correct import
-        //             if let Some(dest_filepath) = match_imported_filename_to_path(head_filepaths, file_import.import_path()) {
-        //                 if match_import_func(&file_import, dest_func) {
-        //                     // if found, create edge
-        //                     let src_filepath = PathBuf::from_str(source_filename).expect("Unable to create pathbuf");
-        //                     if let Some(func_call_vec) = function_calls_in_file(&src_filepath, dest_func.name()).await {
-        //                         // call func in  that takes vec of lines and returns funcdefs
-        //                         let lines = func_call_vec.iter().flat_map(|chunk| chunk.function_calls()).cloned().collect();
-        //                         let source_func_defs_opt = diff_graph.all_file_func_defs().functions_in_file(source_filename);
-        //                         if source_func_defs_opt.is_none() {
-        //                             log::debug!("[incoming_edges] No funcs for file: {}", source_filename);
-        //                             continue;
-        //                         }
-        //                         let source_func_defs = source_func_defs_opt.expect("No source filename found").funcs_for_lines(&lines);
-        //                         for (line_num, source_func_def) in source_func_defs {
-        //                             if source_func_def != dest_func.to_owned() {
-        //                                 graph_elems.add_edge("red",
-        //                                     line_num.to_owned(),
-        //                                     &source_func_def.name(), 
-        //                                     &dest_func.name(),
-        //                                     &source_file_rel,
-        //                                     &dest_file_rel,
-        //                                     "",
-        //                                     "red",
-        //                                     source_func_def.line_start(),
-        //                                     dest_func.line_start()
-        //                                 );
-        //                             }
-        //                         }
-        //                     }
-        //                 }    
-        //             } 
-        //         }
-        //     } 
-        // }
-    // }
     let func_def_identifier_opt = FunctionDefIdentifier::new();
     if func_def_identifier_opt.is_none() {
         log::debug!("[outgoing_edges] Unable to create func def identifier");
@@ -203,7 +24,6 @@ async fn incoming_edges(review: &Review, diff_graph: &DiffGraph, graph_elems: &m
     git_checkout_commit(review, review.pr_head_commit());
     process_func_defs(
         review,
-        &mut funcdef_identifier,
         diff_graph,
         &mut func_call_identifier,
         graph_elems,
@@ -212,24 +32,12 @@ async fn incoming_edges(review: &Review, diff_graph: &DiffGraph, graph_elems: &m
     git_checkout_commit(review, review.base_head_commit());
     process_func_defs(
         review,
-        &mut funcdef_identifier,
         diff_graph,
         &mut func_call_identifier,
         graph_elems,
         "red"
     ).await;
 }
-
-// fn match_import_func(import_obj: &ImportPath, dest_func_info: &FuncDefInfo) -> bool {
-//     log::debug!("[match_import_condition] import_obj.imported = {}, dest_func_info = {:#?}", import_obj.imported(), dest_func_info);
-//     // TODO FIXME - first condition doesn't make sense, it should always be true? - have to check for all calls of this function
-//     match_overlap(&dest_func_info.name(),
-//         &import_obj.imported(),
-//         0.6)
-//         || match_overlap(&dest_func_info.parent(),
-//         &import_obj.imported(),
-//         0.6)
-// }
 
 async fn outgoing_edges(base_filepaths: &Vec<PathBuf>, diff_graph: &DiffGraph,
     graph_elems: &mut MermaidGraphElements, review: &Review) 
@@ -325,7 +133,7 @@ async fn process_func_calls(import_identifier: &mut ImportIdentifier, func_call_
                                                             dest_func_call.function_name(), 
                                                             &source_file_name,
                                                             &possible_file_rel,
-                                                            edge_color,
+                                                            "yellow",
                                                             "",
                                                             src_func_line_number,
                                                             dest_func_def_line);
@@ -360,7 +168,7 @@ async fn process_func_calls(import_identifier: &mut ImportIdentifier, func_call_
                                                                     dest_func_call.function_name(), 
                                                                     &source_file_name,
                                                                     &possible_file_rel,
-                                                                    edge_color,
+                                                                    "yellow",
                                                                     "",
                                                                     src_func_line_number,
                                                                     &dest_func_def_line);
@@ -380,7 +188,7 @@ async fn process_func_calls(import_identifier: &mut ImportIdentifier, func_call_
     // get import and path
 }
 
-async fn process_func_defs(review: &Review, funcdef_identifier: &mut FunctionDefIdentifier,
+async fn process_func_defs(review: &Review,
     diff_graph: &DiffGraph, func_call_identifier: &mut FunctionCallIdentifier,
     graph_elems: &mut MermaidGraphElements, edge_color: &str) 
 {
@@ -397,8 +205,14 @@ async fn process_func_defs(review: &Review, funcdef_identifier: &mut FunctionDef
         } else {
             func_defs = dest_file_hunks.added_hunks();
         }
+        let mut repeated_funcs = HashSet::<String>::new();
         for dest_func in func_defs {
             if let Some(dest_func_name) = dest_func.function_name() {
+                if repeated_funcs.get(dest_func_name).is_some()  {
+                    continue;
+                } else {
+                    repeated_funcs.insert(dest_func_name.to_string());
+                }
                 if let Some(dest_funcdef_line) = dest_func.line_number() {
                     if let Some(possible_filepaths) = 
                     function_calls_search(review, dest_func_name, &dest_lang)
@@ -443,7 +257,7 @@ async fn process_func_defs(review: &Review, funcdef_identifier: &mut FunctionDef
                                         &source_filename,
                                         &dest_file_rel,
                                         "",
-                                        edge_color,
+                                        "yellow",
                                         src_func_def.line_start(),
                                         dest_funcdef_line);
                                     }
